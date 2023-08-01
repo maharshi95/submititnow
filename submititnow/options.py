759fc00bdab6ca092f694168a0db42d6ac7da367
maharshi95 <mgor@cs.umd.edu>
2023-08-01 13:20:54 -0400

    Added support for all major slurm arguments. * Implemented custom action for --nodelist * Removed `slurm` prefix from the option strings * Changed `exp_name` to `exp-name` * Added gpu_matmul.py in examples for GPU testing * Bumping the version to 0.9.2

--- submititnow/options.py
+++ submititnow/options.py
@@ -2,31 +2,109 @@ import argparse
 from typing import Dict, Any
 
 
+class SlurmAdditionalArgAction(argparse.Action):
+    def __init__(self, check_func, *args, **kwargs):
+        """
+        argparse custom action.
+        :param check_func: callable to do the real check.
+        """
+        self._check_func = check_func
+        super(SlurmAdditionalArgAction, self).__init__(*args, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string):
+        if isinstance(values, list):
+            values = [self._check_func(parser, v) for v in values]
+        else:
+            values = self._check_func(parser, values)
+        if option_string.startswith("--"):
+            option_string = option_string[2:]
+        setattr(namespace, self.dest, {option_string: values})
+
+
 def add_slurm_arguments(parser: argparse.ArgumentParser):
     slurm_group = parser.add_argument_group("SLURM parameters")
     slurm_group.add_argument(
-        "--slurm_profile", default=None, help="SubmititNow profile for SLURM."
-    )
-    slurm_group.add_argument("--slurm_account", default=None, help="SLURM account")
-    slurm_group.add_argument("--slurm_partition", default=None, help="SLURM partition")
-    slurm_group.add_argument("--slurm_qos", default=None, help="SLURM qos")
-    slurm_group.add_argument(
-        "--slurm_mem", default=None, help="SLURM memory requirement"
+        "--profile",
+        default=None,
+        help="SubmititNow profile for SLURM.",
+        dest="slurm_profile",
     )
     slurm_group.add_argument(
-        "--slurm_gres", default=None, help="SLURM GPU Resource requirement"
+        "--account", default=None, help="SLURM account", dest="slurm_account"
     )
     slurm_group.add_argument(
-        "--slurm_time", default=None, help="SLURM time requirement"
+        "--partition", default=None, help="SLURM partition", dest="slurm_partition"
+    )
+    slurm_group.add_argument("--qos", default=None, help="SLURM qos", dest="slurm_qos")
+    slurm_group.add_argument(
+        "--mem", default=None, help="SLURM memory requirement", dest="slurm_mem"
+    )
+    slurm_group.add_argument(
+        "--gres", default=None, help="SLURM GPU Resource requirement", dest="slurm_gres"
+    )
+    slurm_group.add_argument(
+        "--time", default=None, help="SLURM time requirement", dest="slurm_time"
+    )
+    slurm_group.add_argument(
+        "--nodes",
+        default=1,
+        help="SLURM nodes requirement",
+        dest="slurm_nodes",
+        type=int,
+    )
+    slurm_group.add_argument(
+        "--ntasks-per-node",
+        default=None,
+        help="SLURM ntasks per node",
+        dest="slurm_ntasks_per_node",
+        type=int,
+    )
+    slurm_group.add_argument(
+        "--cpus-per-task",
+        default=None,
+        help="SLURM cpus per task",
+        dest="slurm_cpus_per_task",
+        type=int,
+    )
+    slurm_group.add_argument(
+        "--cpus-per-gpu",
+        default=None,
+        help="SLURM cpus per gpu",
+        dest="slurm_cpus_per_gpu",
+        type=int,
+    )
+    slurm_group.add_argument(
+        "--gpus-per-node",
+        default=None,
+        help="SLURM gpus per node",
+        dest="slurm_gpus_per_node",
+        type=int,
+    )
+    slurm_group.add_argument(
+        "--gpus-per-task",
+        default=None,
+        help="SLURM gpus per task",
+        dest="slurm_gpus_per_task",
+        type=int,
+    )
+
+    # Additional arguments
+    slurm_group.add_argument(
+        "--nodelist",
+        default=None,
+        help="SLURM nodelist",
+        action=SlurmAdditionalArgAction,
+        check_func=lambda parser, value: value,
+        dest="slurm_additional_parameters",
     )
     return parser
 
 
 def add_submititnow_arguments(parser: argparse.ArgumentParser):
     submititnow_group = parser.add_argument_group("SubmititNow parameters")
-    submititnow_group.add_argument("--exp_name", default=None, help="Experiment Name.")
+    submititnow_group.add_argument("--exp-name", default=None, help="Experiment Name.")
     submititnow_group.add_argument(
-        "--submititnow_dir", default=None, help="Root directory for submititnow."
+        "--submititnow-dir", default=None, help="Root directory for submititnow."
     )
     return parser
 
